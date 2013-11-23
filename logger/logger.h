037312c8b603d87518880bd42ec7e44c43392ac4
Stephen Kuenzli <skuenzli@qualimente.com>
2013-11-23 16:56:32 -0700

    Initial Commit

---
+++ logger/logger.h
@@ -0,0 +1,13 @@
+//
+//  logger.h
+//  logger
+//
+//  Created by Stephen Kuenzli on 11/23/13.
+//  Copyright (c) 2013 Weblog-NG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface logger : NSObject
+
+@end
---
+++ logger/logger.m
@@ -0,0 +1,13 @@
+//
+//  logger.m
+//  logger
+//
+//  Created by Stephen Kuenzli on 11/23/13.
+//  Copyright (c) 2013 Weblog-NG. All rights reserved.
+//
+
+#import "logger.h"
+
+@implementation logger
+
+@end
