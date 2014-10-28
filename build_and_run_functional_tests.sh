e627e9b81c983482abb2cbc065243928a9895bc1
Stephen Kuenzli <skuenzli@weblogng.com>
2014-10-27 20:58:57 -0700

    Update build scripts to use WNGLogger workspace.

--- build_and_run_functional_tests.sh
+++ build_and_run_functional_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-xctool.sh -workspace logger.xcworkspace -scheme logger -sdk iphonesimulator \
+xctool.sh -workspace WNGLogger.xcworkspace -scheme logger -sdk iphonesimulator \
 	clean \
 	test -only StressTests:FunctionalTests/test_sending_metrics_over_http \
 	test -only StressTests:FunctionalTests/test_connection_delegate_invokes_success_for_good_url
